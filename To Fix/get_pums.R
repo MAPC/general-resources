fbb5dcd6a01155629972597184fbfe1106e65326
Brandon Stanaway <104210394+brandonstanaway@users.noreply.github.com>
2026-02-05 14:11:33 -0500

    Update get_pums.R
    Remove non-general variable cleaning

--- To Fix/get_pums.R
+++ To Fix/get_pums.R
@@ -26,42 +26,11 @@ knitr::opts_knit$set(root.dir = pums_path)
 set.seed(351)
 
 source("C:/Users/ziacovino/Desktop/statewidehousingplan/PUMS_Analysis/PUMS_Helper_Functions.R")
-
-xw <- read.csv(paste0(rwt_path, "PUMS_data/PUMA10_RPA_crosswalk.csv")) |> 
-  mutate(PUMA = str_pad(PUMA, 5, "left", pad = 0))
-
-# Load MUnicipality to PUMA Crosswalk
-muni2puma<-read_csv("K:/DataServices/Projects/Current_Projects/Housing/StatewideHousingPlan/04_Analysis/Data/Working/Reweighter/PUMS_data/pums_muni_inter.csv")%>%
-  select(TOWN_ID, PUMACE10) |> 
-  dplyr::rename(muni_id = TOWN_ID)  
-
-
+ 
 mapc_pums_cleaning<-function(vintage_year, addl_vars = c()){  
   
   # Set PUMS vintage final year for {tidycensus} API query.
   vintage <- vintage_year
-  
-  # List of PUMS variables
-  #' variable_list <- c(
-  #'   #'RT', 
-  #'  'PUMA', 'ADJHSG',
-  #'  'ADJINC',
-  #'  'NP', #'TYPEHUGQ',
-  #'  'BDSP','RMSP','CPLT',
-  #'  'HHLDRAGEP',
-  #'  'HHLDRRAC1P','HHLDRHISP', 'HHT', 'HHT2', 
-  #'  'HINCP', 'HUGCL', 'HUPAC', 'HUPAOC',
-  #'                    'HUPARC', 'LNGI', 'MULTG', 'NPF', 'NPP', 'NR','NRC', 'PARTNER', 'PSF','R18',
-  #'                    
-  #'  'AGEP',
-  #'  'RELSHIPP','RAC1P','OC',
-  #'  'SFN','SFR',
-  #'  'TEN', "SEX", "PINCP", 
-  #'  "ESR",
-  #'                    "MV", 
-  #'  "GRPIP", 
-  #'  "OCPIP", "SCH", "SCHG")
-  
   #All of the variables pre-cleaned
   if(vintage_year >= 2012){
     variable_list<-c('PUMA', 'ADJINC', 'NP', 'BDSP','HINCP', 'AGEP',
@@ -94,11 +63,7 @@ mapc_pums_cleaning<-function(vintage_year, addl_vars = c()){
       )%>%
       rename(BDSP = BDS)
   }
-  
-  # Load PUMS variables
-  
-  
-  
+
   # General Data Cleaning
   pums_cleaned <- pums |> 
     # Create a household flag for whether there is a child in a household.
@@ -155,16 +120,6 @@ mapc_pums_cleaning<-function(vintage_year, addl_vars = c()){
         # If no information is present default to not in the laborforce.
         .default = 0
       ),
-      # Create a grouping variable for types of relationships to the householder.
-      # NOTE: 
-      # (0) indicates a person in a household that is related in some way to the householder.
-      # (1) indicates a person in a household unrelated to the householder.
-      # RELSHIPP_Group = case_when(
-      #   RELSHIPP %in% c(21,22,23,24,25,26,27,28,29,30,31,32,33) ~ 0,
-      #   RELSHIPP %in% c(34,35,36) ~ 1,
-      #   # Set a default flag for householders.
-      #   .default = 0
-      # ),
       # Create a grouping variable for owner and renter households.
       TEN_Group = case_when(
         # Owned with a mortage or free and clear -> "Own"
@@ -190,10 +145,6 @@ mapc_pums_cleaning<-function(vintage_year, addl_vars = c()){
         PPB > 2.0 ~ "1",
         PPB <= 2.0 ~ "0"
       ),
-      # Convert SFN into a numeric column. Introduces NAs for people not in subfamilies
-      SFN = as.numeric(SFN),
-      # Replace the NA for non-subfamily members with 0.
-      SFN = replace_na(SFN, 0),
       # Adjust Household income (HINCP) to constant dollars (max of acs year)
       HINCP_adj = as.numeric(HINCP)*as.numeric(ADJINC),
       HINCP_2021 = round(HINCP_adj*(cpi[year==2021, index] / cpi[year==vintage_year, index]), 2),
@@ -206,29 +157,23 @@ mapc_pums_cleaning<-function(vintage_year, addl_vars = c()){
         HINCP_2021 > 248000 ~ ">$248,000",
         .default = "0"
       ),
-      # # Householder Race Groupings. 
-      # HHLDRRACE_Group = case_when(
-      #   HHLDRRAC1P == "1" ~ "White Alone",
-      #   HHLDRRAC1P == "2" ~ "Black or African American Alone",
-      #   HHLDRRAC1P %in% c("3", "4", "5") ~ "AIAN Alone",
-      #   HHLDRRAC1P == "6" ~ "Asian Alone",
-      #   HHLDRRAC1P == "7" ~ "NHPI Alone",
-      #   HHLDRRAC1P == "8" ~ "SOR Alone",
-      #   HHLDRRAC1P == "9" ~ "Two or More Races Alone",
-      #   .default = "Unknown"
-      # ),
-      # # Householder Latine Groupings
-      # HHLDRHISP_Group = case_when(
-      #   HHLDRHISP == "01" ~ "Non-Hispanic",
-      #   HHLDRHISP != "01" ~ "Hispanic",
-      #   .default = "Unknown"
-      # ),
-      # # Create a flag for whether a person in a household is a roommate or housemate
-      # roommate_flag = case_when(
-      #   RELSHIPP == 34 ~ 1,
-      #   RELSHIPP != 34 ~ 0,
-      #   .default = 0
-      # ),
+      # # Person Race Groupings. 
+       RACE_Group = case_when(
+         RAC1P == "1" ~ "White Alone",
+         RAC1P == "2" ~ "Black or African American Alone",
+         RAC1P %in% c("3", "4", "5") ~ "AIAN Alone",
+         RAC1P == "6" ~ "Asian Alone",
+         RAC1P == "7" ~ "NHPI Alone",
+         RAC1P == "8" ~ "SOR Alone",
+         RAC1P == "9" ~ "Two or More Races Alone",
+         .default = "Unknown"
+       ),
+      # # Person Latine Groupings
+       HISP_Group = case_when(
+         HISP == "01" ~ "Non-Hispanic",
+         HISP != "01" ~ "Hispanic",
+         .default = "Unknown"
+       ),
       # Replace any NAs (for households that own and so dont have a GRPIP and vice versa)
       GRPIP = replace_na(GRPIP, 0),
       OCPIP = replace_na(OCPIP, 0),
@@ -240,76 +185,8 @@ mapc_pums_cleaning<-function(vintage_year, addl_vars = c()){
         GRPIP <= 30 | OCPIP <= 30 ~ "0",
         .default = "0"
       )
-    ) |>
-    # Creating household flags for person variables.
-    group_by(SERIALNO) |> 
-    mutate(
-      # Create a variable which is the sum of the number of children in a household
-      Child_Total = sum(child),
-      # Sum the total number of workers in the household
-      Worker_Total = sum(worker),
-      # Create a variable which is the sum of the number of non-relatives in the household
-      # Non.Relative_Total = sum(RELSHIPP_Group),
-      # # Create a variable that is the sum of roommates in a household
-      # Roommate_Total = sum(roommate_flag),
-      # # Create a column which denotes the presence of subfamilies for a household.
-      SFN_Total = sum(SFN),
-    ) |> 
-    ungroup() |> 
-    mutate(
-      # Create a variable that indicates whether or not a household has children. 
-      child_flag = case_when(
-        Child_Total > 0 ~ "1",
-        Child_Total == 0 ~ "0",
-        .default = "0"
-      ),
-      # Create a variable that indicates whether or not a household has roommates.
-      # roommate_flag = case_when(
-      #   Roommate_Total > 0 ~ "1",
-      #   Roommate_Total == 0 ~ "0",
-      #   .default = "0"
-      # ),
-      # Create a household flag for whether or not the household contains a subfamily.
-      SFN.flag = if_else(SFN_Total > 0, 1, 0),
-      # Create a household flag for presence of a non-relative in the household.
-      # NONRELATIVE.flag = if_else(Non.Relative_Total > 0, 1, 0),
-      # Categorizing worker categories:
-      # (0) no workers
-      # (1) one worker
-      # (2) two workers
-      # (3) three or more workers
-      WRKHH = case_when(
-        Worker_Total == 0 ~ 0,
-        Worker_Total == 1 ~ 1,
-        Worker_Total == 2 ~ 2,
-        Worker_Total >= 3 ~ 3
-      ),
-      # Determine if person is not hhder vs hhder of:
-      # (0) No Workers
-      # (1) One Worker
-      # (2) Two Workers
-      # (3) Three or more workers
-      HHder = case_when(
-        SPORDER == 1 & WRKHH == 0 ~ 0,
-        SPORDER == 1 & WRKHH == 1 ~ 1,
-        SPORDER == 1 & WRKHH == 2 ~ 2,
-        SPORDER == 1 & WRKHH == 3 ~ 3,
-        SPORDER > 1 ~ 99
-      ), 
-      # Determine what type of household the person is heading or not heading:
-      # (1) Householder of Household with Children
-      # (2) Non-Householder in a Household with Children 
-      # (3) Householder of a Household with Multiple Adults and no children 
-      # (4) Non-Householder in a Household with no children
-      # (5) Householder of a Single Person Household
-      HHtype = case_when(
-        Child_Total >= 1 & NP > 1 & HHder != 99 ~ 1,
-        Child_Total >= 1 & NP > 1 & HHder == 99 ~ 2,
-        Child_Total == 0 & NP > 1 & HHder != 99 ~ 3,
-        Child_Total == 0 & NP > 1 & HHder == 99 ~ 4,
-        NP == 1 & HHder != 99 ~ 5
-      )
-    )
+    ) 
+  
   return(pums_cleaned)
   
 }
