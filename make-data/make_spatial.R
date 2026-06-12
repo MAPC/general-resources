77f2ceb745f8b3315200209df4a7315fb974dfc6
Your Name <aderosa@mapc.org>
2026-06-12 15:48:50 -0400

    updating to make a more generic version

--- make-data/make_spatial.R
+++ make-data/make_spatial.R
@@ -78,6 +78,8 @@ race <- read_csv("https://staging.datacommon-react.mapc.org/api/export?token=dat
 
 age <- read_csv("https://staging.datacommon-react.mapc.org/api/export?token=datacommon&database=ds&schema=tabular&table=b15001_educational_attainment_by_age_acs_ct&format=csv&years=2020-24&useMetadataColumns=false")
 
+data <- read_csv("https://datacommon.mapc.org/api/export?token=datacommon&database=ds&schema=tabular&table=b17001_poverty_by_population_acs_ct&format=csv&years=2020-24&useMetadataColumns=false")
+
 race_clean <- race |> 
   filter(acs_year == "2020-24") |> 
   select(ct20_id, acs_year, totpop, totpop_me, nhwhi, nhwhi_me, nhwhi_p, nhwhi_mep) |> 
@@ -88,11 +90,13 @@ age_clean <- age |>
   filter(acs_year == "2020-24") |> 
   select(ct20_id, acs_year, pop18o, pop18o_me, pop65o, pop65ome, pop65o_p, pop65omep)
 
+data_clean <- data |> 
+  select(-c(seq_id, ct10_id, geoid, logrecno)) 
+
 ct_shp <- tract_sf(2020) |> 
   select(ct20_id, muni_name, geometry) |> 
   mutate(ct20_id = as.double(ct20_id))
 
-table_shp <- left_join(race_clean, age_clean, by = "ct20_id") |> 
-  left_join(ct_shp, by = "ct20_id")
+table_shp <- left_join(ct_shp, data_clean, by = "ct20_id")
 
-st_write(table_shp, "K:/DataServices/Projects/Current_Projects/Digital Equity Planning/State-wide Data/Data/Spatial/race_age_2020-24.shp")
+st_write(table_shp, "K:/DataServices/Projects/Current_Projects/PublicHealth/Food_Systems/Online_map/spatial/pov_p.shp")
